5472a0a6bd1b8ced164090dc529c0a996aeacca7
affenzellerkarin <karinaffenzeller@yahoo.de>
2022-01-23 14:42:24 +0100

    Done
    Done

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,6 +2,11 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Parser");
+                System.out.println("Hello Parser");
+        try {
+            new Parser().parseFile("sample1.txt");
+        } catch (ParsingException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/application/Parser.java
+++ src/main/java/application/Parser.java
@@ -32,14 +32,65 @@ public class Parser {
     }
 
     private void parse(Path path) throws ParsingException {
+
+        int count = 0;
+
         try (Stream<String> lineStream = Files.lines(path)) {
             for (String line : lineStream.toList()) {
                 // get the line as string
+                for (int i = 0; i<line.length(); i++) {
+                    elementStack.add(line.charAt(i));
+                    if (line.charAt(i) == '{') {
+                        count++;}
+                    else if (line.charAt(i)=='}') {
+                        count--;
+                        if (count<0) {throw new ParsingException("Error parsing the file. { expected");}
+                    }
+                }
+//                // get the line as string
+                System.out.println(line);
             }
+            if (count>0) {throw new ParsingException("Error parsing the file. } expected");}
+            System.out.println(count);
+            getElementStack();
+//            if (count>0) {throw new ParsingException("Error parsing the file. There is/are " + count + " } missing");}
+//            else if (count<0) {throw new ParsingException("Error parsing the file. There is/are " + (-count) + " { missing");}
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-    }
+
+
+//        try (Stream<String> lineStream = Files.lines(path)) {
+//            for (String line : lineStream.toList()) {
+//                for (int i = 0; i < line.length(); i++) {
+//                    // elementStack.add(line.charAt(i));
+//                    if (line.charAt(i) == '{') {
+//                        elementStack.push(line.charAt(i));
+//                        count++;
+//                    } else if (line.charAt(i) == '}') {
+//                        count--;
+//                        try {
+//                            elementStack.pop();
+//                        } catch (NoSuchElementException e) {
+//                            throw new ParsingException("Error parsing the file. { expected");
+//                        }
+//                    }
+//                }
+////                // get the line as string
+//                System.out.println(line);
+//            }
+//            if (!elementStack.isEmpty()) {
+//                throw new ParsingException("Error parsing the file. } expected");
+//            }
+//            System.out.println(count);
+//            System.out.println(getElementStack());
+//
+//        } catch(IOException e){
+//            System.err.println(e.getMessage());
+//        }
+
+}
 }
 
 
